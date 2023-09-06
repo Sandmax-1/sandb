9a607479f390c84f0b558c3494ec18b9c54c2b28
Sandmax-1 <m.sandman@hotmail.co.uk>
2023-09-06 18:55:56 +0100

    first implementation of reading

--- lsm_tree.py
+++ lsm_tree.py
@@ -1,9 +1,13 @@
-from typing import NamedTuple
+from typing import List, NamedTuple, Optional, Tuple
 from red_black_dict_mod import RedBlackTree
 import sys
+import logging
+from pathlib import Path
+from collections import OrderedDict
 
 from config import ROOT_DIR
 
+LOGGER = logging.logger()
 
 class IndexValue(NamedTuple):
     segment_index: int
@@ -12,34 +16,78 @@ class IndexValue(NamedTuple):
 
 
 class LSMTree:
-    def __init__(self, memtable_max_size: int = 1000):
+    def __init__(self, memtable_max_size: int = 1000, segment_chunk_size_for_indexing: int = 100):
         self.memtable: RedBlackTree = RedBlackTree()
         self.memtable_max_size = memtable_max_size
 
-        self.index: RedBlackTree = RedBlackTree()
-        self.segment_chunk_size_for_indexing = 100
+        self.segment_chunk_size_for_indexing = segment_chunk_size_for_indexing
+        # This is the SStable storage. First value is file path, second value is the 
+        # sparse index for the SStable. This is ordered such that we can look through 
+        # newest to oldest segments. 
+        self.segments: OrderedDict[Path, RedBlackTree] = OrderedDict()
 
         self.segment_index = 0
 
         self.segment_folder_path = ROOT_DIR / "lsm_segments"
         self.segment_folder_path.mkdir(exist_ok=True)
+        
+    def read_from_db(self, key: str) -> Optional[int]:
+        """
+        First try and read from the in-memory memtable. If the key does not exist in there
+        look at the segments stored on disk from latest to oldest. As the segments are stored
+        in time order and an append of a new key counts as an update we can stop as soon as 
+        we find our key. 
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            Optional[int]: _description_
+        """
+
+        try: 
+            value = self.memtable[key]
+            return value
+        except KeyError:
+            LOGGER.info(f'key: {key} not in in memory memtable')
+        
+        value = self.search_segments_on_disk(key)
+        
+        return value 
+            
+    def search_segments_on_disk(self, key: str) -> Optional[int]:
+        for filepath, index in self.segments.items():
+            floor_offset, ceil_offset = self.get_floor_ceil_of_key_in_index(key, index)
+            with open(filepath, 'a') as current_segment:
+                current_segment.seek(floor_offset)
+                for line in current_segment:
+                    stored_key, value = line.split(':')
+                    if stored_key == key:
+                        return value
+                    if current_segment.tell() >= ceil_offset:
+                        value = None
+                        break
+        
+        return value
+                
     
     def insert_into_db(self, key: str, value: int) -> None:
         self.memtable.add(key, value)
         if sys.getsizeof(self.memtable) > self.memtable_max_size:
-            self.save_memtable_to_disk()
+            self.flush_memtable_to_disk()
 
-    def save_memtable_to_disk(self):
+    def flush_memtable_to_disk(self):
         index_counter = self.segment_chunk_size_for_indexing
         offset = 0
         segment_file_name = (
             self.segment_folder_path / f"segment_{self.segment_index}.txt"
         )
+        index = RedBlackTree()
 
         with open(segment_file_name, "a") as f:
             for key, value in self.memtable.items:
                 if index_counter == 0:
-                    self.index.add(
+                    index.add(
                         IndexValue(
                             segment_index=self.segment_index, key=key, offset=offset
                         )
@@ -51,9 +99,11 @@ class LSMTree:
                 offset += len(record)
                 index_counter -= 1
 
+        self.memtable = RedBlackTree()
+        self.segments.update({segment_file_name: index})
         self.segment_index += 1
         
-    def get_floor_ceil_of_key_in_index(self, inputted_key: str):
+    def get_floor_ceil_of_key_in_index(self, inputted_key: str, index: RedBlackTree) -> Tuple(int, int):
         """
         Not very performant algorithm for looping through our red black tree index
         to find the boundary where the key to search for is. 
@@ -68,7 +118,7 @@ class LSMTree:
         floor = None
         ceil = None
         prev_value = None 
-        for key, value in self.index.items():
+        for key, value in index.items():
             if key == inputted_key:
                 return (value, value)
             elif inputted_key < key:
