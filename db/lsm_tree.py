1f127a9bfc07304526a32cd2e34ceb59a89b3b58
Sandmax-1 <m.sandman@hotmail.co.uk>
2024-08-15 02:35:49 +0100

    precommit issues

--- db/lsm_tree.py
+++ db/lsm_tree.py
@@ -1,12 +1,10 @@
-from dataclasses import dataclass
-from io import TextIOWrapper
-from typing import List, NamedTuple, Optional, Tuple
-from red_black_dict_mod import RedBlackTree
-import sys
 import logging
-from pathlib import Path
 from collections import OrderedDict
+from pathlib import Path
+from typing import Any, Tuple
+
 from numpy import inf
+from red_black_dict_mod import RedBlackTree
 
 from db.config import ROOT_DIR
 
@@ -29,12 +27,14 @@ class LSMTree:
         self.segment_folder_path = ROOT_DIR / "lsm_segments"
         self.segment_folder_path.mkdir(exist_ok=True)
 
-    def read_from_db(self, key: str) -> Optional[int]:
+    def read_from_db(self, key: str) -> str | None:
         """
-        First try and read from the in-memory memtable. If the key does not exist in there
-        look at the segments stored on disk from latest to oldest. As the segments are stored
-        in time order and an append of a new key counts as an update we can stop as soon as
-        we find our key.
+        First try and read from the in-memory memtable.
+        If the key does not exist in there
+        look at the segments stored on disk from latest to oldest.
+        As the segments are stored
+        in time order and an append of a new key counts
+        as an update we can stop as soon as we find our key.
 
         Args:
             key (str): _description_
@@ -44,8 +44,8 @@ class LSMTree:
         """
 
         try:
-            value = self.memtable[key]
-            return value
+            return str(self.memtable[key])
+
         except KeyError:
             logging.info(f"key: {key} not in in memory memtable")
 
@@ -53,7 +53,7 @@ class LSMTree:
 
         return value
 
-    def search_segments_on_disk(self, key: str) -> Optional[int]:
+    def search_segments_on_disk(self, key: str) -> str | None:
         for filepath, index in self.segments.items():
             floor_offset, ceil_offset = self.get_floor_ceil_of_key_in_index(key, index)
             with open(filepath, "r") as current_segment:
@@ -64,18 +64,18 @@ class LSMTree:
                     stored_key, value = line.split(":")
                     if stored_key == str(key):
                         return value.strip()
-                    if curr_offset >= ceil_offset:
-                        value = None
+                    if ceil_offset and curr_offset >= ceil_offset:
+                        value = ""
                         break
 
         return value
 
-    def insert_into_db(self, key: str, value: int) -> None:
+    def insert_into_db(self, key: Any, value: Any) -> None:
         if len(self.memtable) >= self.memtable_max_size:
             self.flush_memtable_to_disk()
         self.memtable.add(key, value)
 
-    def flush_memtable_to_disk(self):
+    def flush_memtable_to_disk(self) -> None:
         index_counter = self.segment_chunk_size_for_indexing
         segment_file_name = (
             self.segment_folder_path / f"segment_{self.segment_index}.txt"
@@ -98,7 +98,7 @@ class LSMTree:
 
     def get_floor_ceil_of_key_in_index(
         self, inputted_key: str, index: RedBlackTree
-    ) -> Tuple[int, int]:
+    ) -> Tuple[int, int | None]:
         """
         Not very performant algorithm for looping through our red black tree index
         to find the boundary where the key to search for is.
@@ -114,6 +114,7 @@ class LSMTree:
         ceil = None
         prev_value = 0
         for key, value in index.items():
+            value = int(value)
             if key == inputted_key:
                 return (value, value)
             elif inputted_key < key:
@@ -121,8 +122,9 @@ class LSMTree:
                 break
             prev_value = value
 
+        # Value either not in segment, or in final section.
         if ceil is None:
-            floor, ceil = value, None
+            floor = value
 
         return floor, ceil
 
@@ -137,57 +139,56 @@ class LSMTree:
             )
         return int(key_value[0]), key_value[1]
 
-    def compact_segment_files(
-        self,
-        segment_file_path_1: Path,
-        segment_file_path_2: Path,
-        compacted_file_path: Path,
-    ) -> Path:
+    # def compact_segment_files(
+    #     self,
+    #     segment_file_path_1: Path,
+    #     segment_file_path_2: Path,
+    #     compacted_file_path: Path,
+    # ) -> Path:
+    #     def process_next_line(
+    #         segment_file: TextIOWrapper,
+    #     ) -> Tuple[str, bool, Optional[str]]:
+    #         line = segment_file.readline()
+    #         if not line:
+    #             return line, True, None
+    #         key, _ = self.turn_line_into_key_value(line)
+    #         return line, False, key
 
-        def process_next_line(
-            segment_file: TextIOWrapper,
-        ) -> Tuple[str, bool, Optional[str]]:
-            line = segment_file.readline()
-            if not line:
-                return line, True, None
-            key, _ = self.turn_line_into_key_value(line)
-            return line, False, key
+    #     with open(segment_file_path_1, "r") as segment_file_1, open(
+    #         segment_file_path_2, "r"
+    #     ) as segment_file_2, open(compacted_file_path, "a") as compacted_file:
+    #         l1, file_1_empty, k1 = process_next_line(segment_file_1)
+    #         l2, file_2_empty, k2 = process_next_line(segment_file_2)
+    #         while not (file_1_empty or file_2_empty):
+    #             if k1 < k2:
+    #                 compacted_file.write(l1)
+    #                 l1, file_1_empty, k1 = process_next_line(segment_file_1)
 
-        with open(segment_file_path_1, "r") as segment_file_1, open(
-            segment_file_path_2, "r"
-        ) as segment_file_2, open(compacted_file_path, "a") as compacted_file:
-            l1, file_1_empty, k1 = process_next_line(segment_file_1)
-            l2, file_2_empty, k2 = process_next_line(segment_file_2)
-            while not (file_1_empty or file_2_empty):
-                if k1 < k2:
-                    compacted_file.write(l1)
-                    l1, file_1_empty, k1 = process_next_line(segment_file_1)
+    #             elif k2 < k1:
+    #                 compacted_file.write(l2)
+    #                 l2, file_2_empty, k2 = process_next_line(segment_file_2)
 
-                elif k2 < k1:
-                    compacted_file.write(l2)
-                    l2, file_2_empty, k2 = process_next_line(segment_file_2)
+    #             elif k1 == k2:
+    #                 compacted_file.write(l2)
+    #                 l1, file_1_empty, k1 = process_next_line(segment_file_1)
+    #                 l2, file_2_empty, k2 = process_next_line(segment_file_2)
 
-                elif k1 == k2:
-                    compacted_file.write(l2)
-                    l1, file_1_empty, k1 = process_next_line(segment_file_1)
-                    l2, file_2_empty, k2 = process_next_line(segment_file_2)
+    #         remaining_file = segment_file_1 if not file_1_empty else segment_file_2
+    #         final_line = l1 if not file_1_empty else l2
 
-            remaining_file = segment_file_1 if not file_1_empty else segment_file_2
-            final_line = l1 if not file_1_empty else l2
+    #         compacted_file.write(final_line)
+    #         for line in remaining_file.readlines():
+    #             compacted_file.write(line)
 
-            compacted_file.write(final_line)
-            for line in remaining_file.readlines():
-                compacted_file.write(line)
-
-        return compacted_file_path
+    #     return compacted_file_path
 
 
 def merge_segment_files(
-    segment_file_paths: Tuple[Path],
+    segment_file_paths: Tuple[Path, ...],
     merged_file_path: Path,
 ) -> Path:
-
-    # TODO: Need to test whether this is actually more efficient than merging two files over and over.
+    # TODO: Need to test whether this is actually more
+    # efficient than merging two files over and over.
 
     def turn_line_into_key_value(line: str) -> Tuple[int, str]:
         key_value = [x.strip() for x in line.split(":")]
@@ -202,7 +203,6 @@ def merge_segment_files(
         return int(key_value[0]), key_value[1]
 
     with open(merged_file_path, "a") as output_file:
-
         try:
             segment_files = [file.open("r") for file in segment_file_paths]
             lines = [file.readline() for file in segment_files]
@@ -214,7 +214,7 @@ def merge_segment_files(
 
                 # TODO: This will only work with integer keys
                 min_value_index = keys.index(
-                    min(keys, key=lambda x: inf if x == "" else x)
+                    min(keys, key=lambda x: inf if x == "" else x)  # type: ignore
                 )
                 key = keys[min_value_index]
 
