069463652df2181002223099b0c5686989edcdd1
Sandmax-1 <m.sandman@hotmail.co.uk>
2024-08-21 03:40:05 +0100

    add in src...

---
+++ src/sandb/docs/docs/talkscript.md
@@ -0,0 +1,93 @@
+
+Hash Indexes:
+
+- Index stored in memory as a hash map containing keys and offsets to file position.
+- This is very simple, but is actually used by Bitcask (the default storage engine for Riak) a no-sql key-value store
+- This is well suited to a situations where each key is updated frequently, for example lets say products and stock in a shop. 
+Where keys would be the products and stock the value. We have lots of writes (people buying things, restocking), but not too many distict keys.
+You have a lot of writes per key, but you could feasibly keep the keys in memory
+
+Compaction:
+- As I've said we only ever append to a file, so how do we avoid filling up the disk? 
+- A good solution to this is splitting up the log file into segments. 
+- When the segment file reaches a certain size we close the file and start writing to a new file. 
+- This allows for compaction of older segments. This entails throwing away duplicate keys in the segments. As 
+we are only appening to files the latest record for a given key is the current record for the file so any older records can be thrown away 
+- In these update heavy scenarios after compaction the segments should have significantly reduced in size and it is possible to merge multiple segments together. 
+- Segments are never modified after they have been written so the compaction process writes to a new segment file.
+- As such the compaction process can be run in a background thread so we can maintain the dbs availability and read from the old segment files during this process. Then once complete we switch over to the new segment files. Then the old segment files can be deleted.
+- Each segment now has it's own in memory hash table and when searching for a value for a given key we search each hash map starting rom the newest to the oldest. 
+- To delete a record a special tombstone record iis appended that will get cleaned up in the compaction process
+- If our process/machine crashes we will lose our in memory hash maps. These can be recreated directly from the segment files, however this can take some time if the segment files are large. So some dbs will speed up recovery by storing a snapshot of the segments hashmap on disk for quicker recovery.
+
+Append log:
+Append only logs can seem wasteful at first glance,why don't we just update the file in place overwriting old values? However they provide some benefits:
+
+- Appending and segment merging are sequential write operations which are generally much faster than random writes.
+
+- Concurrency and crash recovery are much simpler if isegment files are append only, or immutable. You don't have to worry about the case where a crash happend while a value was being overwritten. 
+
+- Merging old segments avoids the problem of data files getting fragmented over time.
+
+Downsides:
+- In order for this db to work we need to be able to keep the hashmaps in memory. If we have a very large number of keys this can become infeasible. In principle you could keep the hashmaps on disk, but it is difficult to make on disk hash tables perform efficiently. They require a lot of random access I/O, hash collisions are difficult to manage and they can be expensive to grow when full. 
+
+- Range queries are not efficient as you have to look up key individually in the hash maps. 
+
+
+How can we solve these downsides? 
+
+In the hash indexing example we just write to file with every new request. We can make a simple change to the way we store our data. 
+
+If instead we assert that our segment files must be sorted by key. We call this format a sorted string table, or SStable for short. In this format we cannot append new key-value pers to the segment files immediately as they can come in any order. We will see shortly how we overcome this. 
+
+SStables have some big advantages over log segments with hash indexes. 
+
+- Merging SSTables is simple and efficient, even if our segments are bigger than the available memory. We essentially use a mergesort algorithm illustrated below
+    - if key appears in multiple segments take the latest segment as segments are written sequentially.
+
+![Alt text](src/sandb/docs/images/compaction.png)
+
+- In order to find a particular key in the file we no longer need to keep an index of all keys in memory. Say you are looking for a specific key, then as long as you know some bounding keys you can search in between as the log is ordered by key. 
+
+![Alt text](src/sandb/docs/images/sstable.png)
+
+- You still need an in memory index to tell you the offsets of some keys, but this can be very sparse: one key for every few kilobytes of segment file is sufficient.
+
+- We can even compress the block of memory in between our sparse keys as we will always have to scan the whole block. 
+
+Constructing and maintaining SSTables:
+
+This is all well and good , but how do we get our data sorted by key in the first place? As writes can come in in any order. 
+
+We will maintain an inmemory soreted data structure. There are lots of well known tree data structures that you can use such as red-black, or AVL trees. 
+
+We can now make our storage engine function like this: 
+- When a write comes in add it to the in memory balanced tree structure. We will call this structure a memtable. 
+- When the memtable reaches a certain size (typically a few MB) we write out to disk. This is efficient as the tree already maintains a sorted order. While we are writing to disk a new memtable is created and any writes will get stored in the new memtable
+- In order to perform a read. First check the in memory memtable then if it is not stored in there look at each segment on disk from newest to oldest. 
+- From time to perform merging and compaction to combine segment files and discard deleted or overwritten values.
+
+- One problem that this storage scheme uffers from is if the db crashes we lose the latest memtable. This can be mitigated by implementing a write ahead log. I.e. every time a new key comes in we first append to a seperate log which is unordered then write to the memtable. It doesn't matter that this log is unordered as it's only purpose is to restore the memtable in the event of a db crash. Delete the corresponding log every time we write a memtable to a segment file. 
+
+
+This scheme is called an LSM tree and is used by many different dbs namely LevelDB, RocksDB, Cassandra and HBase. 
+
+Optimisations:
+- One thing you may notice is that if we attempt to read from the db, but the record does not exist in any of the segments we have to check the memtable and then all the segments back to the oldest segment before we can be sure that a key does not exist in the db. A solution to this problem is to use a data structure called a bloom filter. This is similar to a hash table, but does not store the actual values in the rather it will store whether the key is in the set or not. Bloom filters assure no false negatives and can be tweaked to have a very low false posutive rate. Looking at the bloom filter will tel us whether the key is in the db or not thus saving unecessary disk reads. 
+
+- Talk about compaction and levelled/size-tiered.
+
+The log structured indexes discussed so far are gaining traction, but they are not the most common type of index. The most common indeing structure is very different: the B-tree:
+
+B-trees have been around for a long time and are used are the standard index implementation in almost all relational dbs and many nonrelational databases. 
+
+Like LSM trees B-trees keep key-value pairs sorted by by key which allow efficient lookups and range queries. 
+
+However log structured indexes break down the database inoto variable sized segments and always write a segment sequentially. B-trees break the db down into fixed sized segements typically 4KB in size and read or write one page at a time. This designn corrresponds more closely to the underlying hardware as disks are also arranged in fixed sized blocks. 
+
+![Alt text](src/sandb/docs/images/b-tree.png)
+
+- Each page can be identified using an address, or location which allows one page to refer to anohther page (similar to a pointer, but on disk)
+- One page is designated as the root of the tree and any look up starts from here filtering down the pages. WHere each page is responsible for a continuous range of keys.
+- Go through example. 
---
+++ src/sandb/indexes/lsm_tree.py
@@ -0,0 +1,261 @@
+import logging
+from collections import OrderedDict
+from pathlib import Path
+from typing import Protocol, Tuple, TypeVar
+
+from numpy import inf
+from sortedcontainers import SortedDict
+
+from sandb.config import ROOT_DIR
+
+Comparable = TypeVar("Comparable", bound="ComparableType")
+T = TypeVar("T")
+
+
+class ComparableType(Protocol):
+    """Protocol for annotating comparable types."""
+
+    def __lt__(self: Comparable, other: Comparable) -> bool:
+        pass
+
+
+class LSMTree:
+    def __init__(
+        self, memtable_max_size: int = 1000, segment_chunk_size_for_indexing: int = 100
+    ):
+        self.memtable: SortedDict[Comparable, T] = SortedDict()
+        self.memtable_max_size = memtable_max_size
+
+        self.segment_chunk_size_for_indexing = segment_chunk_size_for_indexing
+        # This is the SStable storage. First value is file path, second value is the
+        # sparse index for the SStable. This is ordered such that we can look through
+        # newest to oldest segments.
+        self.segments: OrderedDict[Path, SortedDict] = OrderedDict()
+
+        self.segment_index = 0
+
+        self.segment_folder_path = ROOT_DIR / "lsm_segments"
+        self.segment_folder_path.mkdir(exist_ok=True)
+
+    def read_from_db(self, key: Comparable) -> str | None:
+        """
+        First try and read from the in-memory memtable.
+        If the key does not exist in there
+        look at the segments stored on disk from latest to oldest.
+        As the segments are stored in time order and an append of a
+        new key counts as an update we can stop as soon as we find our key.
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            Optional[int]: _description_
+        """
+
+        try:
+            return str(self.memtable[key])
+
+        except KeyError:
+            logging.info(f"key: {key} not in in memory memtable")
+
+        value = self.search_segments_on_disk(key)
+
+        return value
+
+    def search_segments_on_disk(self, key: Comparable) -> str | None:
+        for filepath, index in self.segments.items():
+            floor_offset, ceil_offset = self.get_floor_ceil_of_key_in_index(key, index)
+            with open(filepath, "r") as current_segment:
+                current_segment.seek(floor_offset)
+                curr_offset = floor_offset
+                for line in current_segment:
+                    curr_offset += len(line)
+                    stored_key, value = line.split(":")
+                    if stored_key == str(key):
+                        return value.strip()
+                    if ceil_offset and curr_offset >= ceil_offset:
+                        value = ""
+                        break
+
+        return value
+
+    def insert_into_db(self, key: Comparable, value: T) -> None:
+        if len(self.memtable) >= self.memtable_max_size:
+            self.flush_memtable_to_disk()
+        self.memtable.update({key: value})
+
+    def flush_memtable_to_disk(self) -> None:
+        index_counter = self.segment_chunk_size_for_indexing
+        segment_file_name = (
+            self.segment_folder_path / f"segment_{self.segment_index}.txt"
+        )
+        index: SortedDict[Comparable, int] = SortedDict()
+
+        with open(segment_file_name, "a") as f:
+            for key, value in self.memtable.items():
+                if index_counter == 0:
+                    offset = f.tell()
+                    index.update({key: offset})
+                    index_counter = self.segment_chunk_size_for_indexing
+
+                f.write(f"{key}: {value}\n")
+                index_counter -= 1
+
+        self.memtable = SortedDict()
+        self.segments.update({segment_file_name: index})
+        self.segment_index += 1
+
+    def get_floor_ceil_of_key_in_index(
+        self, inputted_key: Comparable, index: SortedDict[Comparable, int]
+    ) -> Tuple[int, int | None]:
+        """
+        Not very performant algorithm for looping through our SortedDict index
+        to find the boundary where the key to search for is.
+
+        I.e. if our tree looks something like:
+        {a: 100
+        h: 200
+        q: 300
+        z: 400}
+        then the boundaries if we try and find key j would be a and h.
+        """
+        floor = 0
+        ceil = None
+        prev_value = 0
+        for key, value in index.items():
+            value = int(value)
+            if key == inputted_key:
+                return (value, value)
+            elif inputted_key < key:
+                floor, ceil = prev_value, value
+                break
+            prev_value = value
+
+        # Value either not in segment, or in final section.
+        if ceil is None:
+            floor = value
+
+        return floor, ceil
+
+    @staticmethod
+    def turn_line_into_key_value(line: str) -> Tuple[int, str]:
+        key_value = [x.strip() for x in line.split(":")]
+        if len(key_value) != 2:
+            raise Exception(
+                f"""Line was parsed incorrectly.
+                            Expected a key value pair seperated by a colon.
+                            Received: {line}"""
+            )
+        return int(key_value[0]), key_value[1]
+
+    # def compact_segment_files(
+    #     self,
+    #     segment_file_path_1: Path,
+    #     segment_file_path_2: Path,
+    #     compacted_file_path: Path,
+    # ) -> Path:
+    #     def process_next_line(
+    #         segment_file: TextIOWrapper,
+    #     ) -> Tuple[str, bool, Optional[str]]:
+    #         line = segment_file.readline()
+    #         if not line:
+    #             return line, True, None
+    #         key, _ = self.turn_line_into_key_value(line)
+    #         return line, False, key
+
+    #     with open(segment_file_path_1, "r") as segment_file_1, open(
+    #         segment_file_path_2, "r"
+    #     ) as segment_file_2, open(compacted_file_path, "a") as compacted_file:
+    #         l1, file_1_empty, k1 = process_next_line(segment_file_1)
+    #         l2, file_2_empty, k2 = process_next_line(segment_file_2)
+    #         while not (file_1_empty or file_2_empty):
+    #             if k1 < k2:
+    #                 compacted_file.write(l1)
+    #                 l1, file_1_empty, k1 = process_next_line(segment_file_1)
+
+    #             elif k2 < k1:
+    #                 compacted_file.write(l2)
+    #                 l2, file_2_empty, k2 = process_next_line(segment_file_2)
+
+    #             elif k1 == k2:
+    #                 compacted_file.write(l2)
+    #                 l1, file_1_empty, k1 = process_next_line(segment_file_1)
+    #                 l2, file_2_empty, k2 = process_next_line(segment_file_2)
+
+    #         remaining_file = segment_file_1 if not file_1_empty else segment_file_2
+    #         final_line = l1 if not file_1_empty else l2
+
+    #         compacted_file.write(final_line)
+    #         for line in remaining_file.readlines():
+    #             compacted_file.write(line)
+
+    #     return compacted_file_path
+
+
+def merge_segment_files(
+    segment_file_paths: Tuple[Path, ...],
+    merged_file_path: Path,
+) -> Path:
+    # TODO: Need to test whether this is actually more
+    # efficient than merging two files over and over.
+
+    def turn_line_into_key_value(line: str) -> Tuple[int, str]:
+        key_value = [x.strip() for x in line.split(":")]
+        if len(key_value) != 2:
+            raise Exception(
+                (
+                    "Line was parsed incorrectly.\n"
+                    "Expected a key value pair seperated by a colon.\n"
+                    f"Received: {line}"
+                )
+            )
+        return int(key_value[0]), key_value[1]
+
+    with open(merged_file_path, "a") as output_file:
+        try:
+            segment_files = [file.open("r") for file in segment_file_paths]
+            lines = [file.readline() for file in segment_files]
+            keys = [turn_line_into_key_value(line)[0] if line else "" for line in lines]
+
+            while any(keys):
+                # This gets us the value we need to add to the merged file provided the
+                # segment_files_list is in order of newest file to oldest.
+
+                # TODO: This will only work with integer keys
+                min_value_index = keys.index(
+                    min(keys, key=lambda x: inf if x == "" else x)  # type: ignore
+                )
+                key = keys[min_value_index]
+
+                output_file.write(lines[min_value_index])
+
+                lines[min_value_index] = segment_files[min_value_index].readline()
+
+                if lines[min_value_index]:
+                    keys[min_value_index] = turn_line_into_key_value(
+                        lines[min_value_index]
+                    )[0]
+                else:
+                    keys[min_value_index] = ""
+
+                breakout = False
+
+                while not breakout:
+                    try:
+                        next_index = keys.index(key)
+                        lines[next_index] = segment_files[next_index].readline()
+                        if lines[next_index]:
+                            keys[next_index] = turn_line_into_key_value(
+                                lines[next_index]
+                            )[0]
+                        else:
+                            keys[next_index] = ""
+
+                    except ValueError:
+                        breakout = True
+        except Exception as e:
+            raise e
+        finally:
+            for file in segment_files:
+                file.close()
+    return merged_file_path
