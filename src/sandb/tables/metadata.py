277f2960d08990f6b731e6282d78d0d163510201
Sandmax-1 <m.sandman@hotmail.co.uk>
2024-08-25 23:22:37 +0100

    add in create function

--- src/sandb/tables/metadata.py
+++ src/sandb/tables/metadata.py
@@ -8,11 +8,20 @@ VALID_DTYPES = str | int
 
 
 class Column(BaseModel):
+    """
+    Holds the metadata about a column. Only needs column name and dtype.
+    dtype only allowed to come from VALID_DTYPES list.
+    """
+
     name: str
     dtype: VALID_DTYPES
 
 
 class TableMetadata(BaseModel):
+    """
+    Holds metadata about a given table.
+    """
+
     name: str
     columns: tuple[Column, ...]
     location: Path
