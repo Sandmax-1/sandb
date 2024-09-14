28d30b27d1937a86d5c5772a30e490e433c8a56d
Sandmax-1 <m.sandman@hotmail.co.uk>
2024-09-14 02:57:38 +0100

    add in table already exists test

--- tests/sandb/tables/test_tables.py
+++ tests/sandb/tables/test_tables.py
@@ -1,10 +1,11 @@
+import pytest
 from pydantic_core import from_json
 
 from sandb.tables.metadata import TableMetadata
-from sandb.tables.table import create
+from sandb.tables.table import TableExistsError, create
 
 
-def test_create(test_table_metadata: TableMetadata) -> None:
+def test_create_happy_path(test_table_metadata: TableMetadata) -> None:
     create(test_table_metadata)
 
     with open(
@@ -17,3 +18,10 @@ def test_create(test_table_metadata: TableMetadata) -> None:
     )
 
     assert actual_metadata == test_table_metadata
+
+
+def test_create_table_folder_already_exists(test_table_metadata: TableMetadata) -> None:
+    (test_table_metadata.location / test_table_metadata.name).mkdir()
+
+    with pytest.raises(TableExistsError):
+        create(test_table_metadata)
