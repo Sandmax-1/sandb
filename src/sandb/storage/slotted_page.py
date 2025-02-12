9a393c195cc2626d88493135b49acba4548ca748
Sandmax-1 <m.sandman@hotmail.co.uk>
2025-02-12 17:52:06 +0000

    add in slotted page and initaial test

--- src/sandb/storage/slotted_page.py
+++ src/sandb/storage/slotted_page.py
@@ -4,6 +4,7 @@ from struct import pack, unpack_from
 from typing import Iterable, Iterator
 
 from sandb.storage.constants import INT_SIZE_IN_BYTES
+from sandb.storage.record import SchemaRecord
 
 
 @dataclass
@@ -17,6 +18,9 @@ class Slot(Iterable[int]):
     record_id: int
     record_pointer: int
     record_length: int
+
+    def to_bytes(self) -> bytes:
+        return pack("<iii", self.record_id, self.record_pointer, self.record_length)
 
     def __iter__(self) -> Iterator[int]:
         """
@@ -38,7 +42,8 @@ class SlottedPageHeader:
     free_space_start: int
     free_space_end: int
     slots: list[Slot]
-    records: list[bytes]
+    is_dirty: bool = False
+    next_row_id: int = 0
     # checksum: int | None  # TODO implement this later
     # TODO: Should I include all my records here once loaded into memory?
     #       Or retrieve from disk using the pointers?
@@ -65,9 +70,6 @@ class SlottedPageHeader:
 
         byte_str = byte_str.ljust(self.free_space_end, b"\0")
 
-        for record in self.records:
-            byte_str += record
-
         return byte_str
 
     @classmethod
@@ -89,7 +91,7 @@ class SlottedPageHeader:
             len_slots,
         ) = unpack_from("<iiii", byte_str, offset)
 
-        offset += INT_SIZE_IN_BYTES * 4
+        offset += INT_SIZE_IN_BYTES * 4 - 1
 
         slots_raw = unpack_from("<" + f"{len_slots * 3}i", byte_str, offset)
         offset += 3 * INT_SIZE_IN_BYTES * len_slots
@@ -101,13 +103,59 @@ class SlottedPageHeader:
             )
             for slot_ind in range(0, 3 * len_slots, 3)
         ]
-        records: list[bytes] = []
-        offset = free_space_end
-
-        for slot in slots:
-            records.append(byte_str[offset : offset + slot.record_length])
-            offset += slot.record_length
 
         return SlottedPageHeader(
-            page_id, free_space_start, free_space_end, slots, records
+            page_id,
+            free_space_start,
+            free_space_end,
+            slots,
         )
+
+
+@dataclass
+class SlottedPage:
+    header: SlottedPageHeader
+    byte_str: bytearray
+    schema_record: SchemaRecord
+    size: int = 150
+
+    @classmethod
+    def from_bytes(cls, byte_str: bytes) -> "SlottedPage":
+        slotted_page_header = SlottedPageHeader.from_bytes(byte_str)
+        schema_offset = slotted_page_header.slots[0].record_pointer
+
+        schema_record = SchemaRecord.from_bytes(byte_str[schema_offset:])
+
+        return SlottedPage(slotted_page_header, bytearray(byte_str), schema_record)
+
+    def add_record(self, record: bytes) -> None:
+        # TODO: How do I make all this atomic?
+        record_length = len(record)
+
+        slot = Slot(
+            record_id=self.header.next_row_id,
+            record_pointer=self.header.free_space_end + 1 - record_length,
+            record_length=record_length,
+        )
+        self.header.next_row_id += 1
+
+        self.header.slots.append(slot)
+
+        self.byte_str[
+            self.header.free_space_start : self.header.free_space_start
+            + 3 * INT_SIZE_IN_BYTES  # noqa
+        ] = slot.to_bytes()
+
+        self.byte_str[
+            self.header.free_space_end - record_length : self.header.free_space_end
+        ] = record
+
+        self.byte_str[4:17] = bytearray(
+            pack(
+                "<iii",
+                self.header.free_space_start + 3 * INT_SIZE_IN_BYTES,
+                self.header.free_space_end - record_length,
+                len(self.header.slots),
+            )
+        )
+        self.is_dirty = True
--- tests/unit/sandb/storage/test_slotted_page.py
+++ tests/unit/sandb/storage/test_slotted_page.py
@@ -1,17 +1,50 @@
 from struct import pack
 
-from sandb.storage.slotted_page import Slot, SlottedPageHeader
+from sandb.storage.record import SchemaRecord
+from sandb.storage.slotted_page import Slot, SlottedPage, SlottedPageHeader
 
 
-def test_slotted_page_serialisation() -> None:
+def test_slotted_page_header_serialisation() -> None:
     slotted_page_header = SlottedPageHeader(
         page_id=0,
         free_space_start=28,
         free_space_end=32,
         slots=[Slot(record_id=0, record_pointer=33, record_length=4)],
-        records=[pack("<i", 84)],
     )
 
     assert slotted_page_header == SlottedPageHeader.from_bytes(
         slotted_page_header.to_bytes()
     )
+
+
+def test_slotted_page_add_record() -> None:
+    header = SlottedPageHeader(
+        page_id=0,
+        free_space_start=28,
+        free_space_end=99,
+        slots=[Slot(record_id=0, record_pointer=100, record_length=4)],
+        next_row_id=1,
+    )
+
+    schema_record = SchemaRecord(
+        0, "table_1", dtypes=[1, 0], col_names=["str_col", "int_col"]
+    )
+    schema_record_bytes = schema_record.to_bytes()
+
+    slotted_page = SlottedPage(
+        header=header,
+        byte_str=bytearray(
+            header.to_bytes().ljust((150 - len(schema_record_bytes)), b"\0")
+            + schema_record_bytes  # noqa
+        ),
+        schema_record=schema_record,
+        size=150,
+    )
+    slotted_page.add_record(pack("<3si", "abc".encode("utf-8"), 2))
+
+    updated_slotted_page = SlottedPage.from_bytes(bytes(slotted_page.byte_str))
+
+    assert updated_slotted_page.header.free_space_start == 40
+    assert updated_slotted_page.header.free_space_end == 92
+    assert updated_slotted_page.header.slots[1] == Slot(1, 93, 7)
+    assert updated_slotted_page.schema_record == schema_record
